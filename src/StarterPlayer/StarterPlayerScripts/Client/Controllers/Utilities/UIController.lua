d5d87e473d556cfd18970a75daf90d0442ccf40c
raiar <raiarcanscript@gmail.com>
2025-10-11 07:01:43 +0200

    fixed UI Controller

--- src/StarterPlayer/StarterPlayerScripts/Client/Controllers/Utilities/UIController.lua
+++ src/StarterPlayer/StarterPlayerScripts/Client/Controllers/Utilities/UIController.lua
@@ -3,18 +3,11 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TweenService = game:GetService("TweenService")
 local MarketPlaceService = game:GetService("MarketplaceService")
-
 -- // Variables -- \\
 local Player = Players.LocalPlayer
 local PlayerUI = Player.PlayerGui
+local GameFolder = ReplicatedStorage:WaitForChild("Game")
 
-local MainUI = PlayerUI:WaitForChild("Main")
-local Frames = MainUI:WaitForChild("Frames")
-local SideButtons1 = MainUI:WaitForChild("SideButtons1")
-local SideButtons2 = MainUI:WaitForChild("SideButtons2")
- 
-
-local Modules = ReplicatedStorage:WaitForChild("Modules")
 
 -- // Bool -- \\
 local isInCooldown = false
@@ -24,7 +17,7 @@ local lastEquippedFrame = nil
 local EffectsTable = {}
 local UIController = {}
 -- // Modules -- \\
-local Libraries = require(Modules:WaitForChild("Library"))
+local Libraries = require(GameFolder:WaitForChild("Library"))
 local ProductIDs = Libraries.Modules.Global.IDs
 
 function OpenFrame(Frame: Frame)
@@ -68,7 +61,7 @@ function UIController:ToggleFrame(Frame: Frame, State: boolean | nil)
 		isInCooldown = false
 	end)
 	if typeof(Frame) == "string" then
-		local foundFrame = Frames:FindFirstChild(Frame)
+		local foundFrame = UIController.Frames:FindFirstChild(Frame)
 
 		if not foundFrame then
 			return
@@ -122,29 +115,6 @@ function UIController:AddHover(UIObject: GuiObject, scaleGoal: number?)
 	local State = Value(nil)
 
 	local function OnHover()
-		if State:get() == false then
-			if EffectsTable[UIObject] then
-				EffectsTable[UIObject] = nil
-				table.remove(EffectsTable, table.find(EffectsTable, UIObject))
-
-				for _, Object in UIObject:GetDescendants() do
-					if Object:IsA("UIGradient") and Object.Name == "Shine" then
-						Object:Destroy()
-					end
-				end
-			end
-		end
-
-		if State:get() == true then
-			local VectorIcon = UIObject:FindFirstChildOfClass("ImageLabel")
-			local BodyImage = UIObject:FindFirstChild("Body")
-
-			if VectorIcon and BodyImage then
-				BodyImage = BodyImage:FindFirstChild("Main")
-				UIController:ToggleShineEffect(UIObject :: Frame, { VectorIcon, BodyImage })
-			end
-		end
-
 		local numberToAnimateTo = State:get() == true and scaleGoal or 1
 
 		Spring.target(UIScale, 0.65, 4, {
@@ -155,7 +125,6 @@ function UIController:AddHover(UIObject: GuiObject, scaleGoal: number?)
 	Give(UIObject)({
 		[OnEvent("MouseEnter")] = function()
 			State:set(true)
-			game.SoundService["UI - Hover 1"]:Play()
 			OnHover()
 		end,
 
@@ -172,7 +141,6 @@ function UIController:ConnectButtonToCallback(UIObject: GuiButton, funct: () -> 
 	end
 
 	UIObject.Activated:Connect(function()
-		game.SoundService["UI Click"]:Play()
 		if funct then
 			funct()
 		end
@@ -215,7 +183,6 @@ function UIController:AddClickEffect(UIObject: GuiButton, UIButton: GuiButton?, 
 	Give(UIButton)({
 		[OnEvent("MouseButton1Down")] = function()
 			State:set(true)
-
 			OnHover()
 		end,
 
@@ -236,65 +203,11 @@ function UIController:Start()
 	UIController.Spring = Libraries.Modules["Resources"].Spring
 	UIController.Chemical = Libraries.Modules["Resources"].Chemical
 
-	for _, v in Frames:GetDescendants() do
-		if v:IsA("TextButton") or v:IsA("ImageButton") then
-			if v.Name ~= "Close" then
-				continue
-			end
+	local MainUI = PlayerUI:WaitForChild("Main")
+	local Frames = MainUI:WaitForChild("Frames")
 
-			UIController:ConnectButtonToCallback(v, function()
-				local MainFrame = v.Parent.Parent
 
-				UIController:ToggleFrame(MainFrame, false)
-			end)
-		end
-	end
-	
-	local function AddAnim(Frame: Frame, isBTN: boolean)
-		UIController:AddHover(Frame, 1.06)
-		
-		if isBTN == true then
-			UIController:AddClickEffect(Frame.Frame, Frame.Frame.BUY, .95)
-		end
-		
-		if not isBTN then
-			UIController:AddClickEffect(Frame, Frame.TextButton, .95)
-			
-			UIController:ConnectButtonToCallback(Frame.TextButton, function()
-				UIController:ToggleFrame(Frame.Name)
-			end)
-		end
-	end
-	
-	for i, Frame in SideButtons1:GetChildren() do
-		if not Frame:IsA("Frame")then
-			continue
-		end
-		if string.find(Frame.Name, "BTN") then
-			AddAnim(Frame, true)
-		else
-			for i, mainFrame in Frame:GetChildren()do
-				if not mainFrame:IsA("Frame") then
-					continue
-				end
-				AddAnim(mainFrame, false)
-			end
-		end
-	end
-	
-	--> 2x Speed Animation
-	UIController:AddHover(SideButtons2["2XSPEED"], 1.08)
-	UIController:AddClickEffect(SideButtons2["2XSPEED"], SideButtons2["2XSPEED"].BUY, .9)
-	
-	
-	
-	--> Conveyor Button Animations
-	UIController:AddHover(MainUI.ConveyorBTN, 1.08)
-	UIController:AddClickEffect(MainUI.ConveyorBTN, MainUI.ConveyorBTN.TextButton, .9)
-	
-	UIController:ConnectButtonToCallback(SideButtons1.LevelBTN.Frame.BUY, function()
-		MarketPlaceService:PromptProductPurchase(Player, ProductIDs["Skip Level"])
-	end)
+	UIController.Frames = Frames
 end
 
 return UIController
