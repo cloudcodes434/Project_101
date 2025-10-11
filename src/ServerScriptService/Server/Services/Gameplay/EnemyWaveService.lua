0c4fe97d67d23af75323363f949fac2e106e93ac
raiar <raiarcanscript@gmail.com>
2025-10-11 07:18:42 +0200

    waveservice + enemywaveserrvice

---
+++ src/ServerScriptService/Server/Services/Gameplay/EnemyWaveService.lua
@@ -0,0 +1,26 @@
+
+-- // Services -- \\
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- // Variables -- \\ 
+local GameFolder = ReplicatedStorage:WaitForChild("Game")
+local Libraries = GameFolder:WaitForChild("Libraries")
+-- // Modules -- \\
+local Library = require(Libraries:WaitForChild("Library"))
+
+local Service = {}
+
+function Service:Spawn()
+    
+end
+
+function Service.GetWave(): number
+    
+end
+
+function Service:Start()
+    Service.WaveService = Library:GetService("WaveService", true)
+end
+
+return Service
